f6dbcbe82d0b0cf92c49e1e851e58a558d833e7e
Thilanga Pitigala <pittado@gmail.com>
2020-07-02 14:49:08 +1000

    Updated to handle token save on failiours

--- run.php
+++ run.php
@@ -1,10 +1,10 @@
 <?php
 
-use Keboola\GenericExtractor\Exception\ApplicationException;
+use Keboola\GenericExtractor\Configuration\Extractor;
 use Keboola\GenericExtractor\Exception\UserException;
 use Keboola\GenericExtractor\Executor;
 
-require_once(__DIR__ . "/vendor/autoload.php");
+require_once(__DIR__."/vendor/autoload.php");
 
 // initialize logger
 $logger = new Monolog\Logger("logger");
@@ -12,33 +12,69 @@ $stream = fopen('php://stdout', 'r');
 $logger->pushHandler(new \Monolog\Handler\StreamHandler($stream));
 //$logger->debug("Starting up");
 
+function moveNewStateFile(\Monolog\Logger $logger)
+{
+    // do we have the new state.json in /out/ dir?
+    $arguments = getopt("d::", ["data::"]);
+    if (!isset($arguments["data"])) {
+        throw new UserException('Data folder not set.');
+    }
+
+    $dataDir = $arguments["data"];
+
+    // replace the state.json in /in/ dir
+    // read the in/state.json
+    $configuration = new Extractor($dataDir, $logger);
+    echo "\n";
+    echo "\n";
+    echo 'Save new auth file';
+    $configuration->saveConfigMetadata($configuration->getFullConfigArray());
+}
+
 try {
     $executor = new Executor($logger);
     $executor->run();
-} catch (UserException $e) {
-    $logger->error($e->getMessage(), (array)$e->getData());
-    exit(1);
-} catch (\Keboola\Juicer\Exception\UserException $e) {
-    $logger->error($e->getMessage(), (array)$e->getData());
-    exit(1);
-} catch (ApplicationException $e) {
-    $logger->error($e->getMessage(), (array)$e->getData());
-    exit($e->getCode() > 1 ? $e->getCode() : 2);
 } catch (Exception $e) {
-    if ($e instanceof \GuzzleHttp\Exception\RequestException
-        && $e->getPrevious() instanceof UserException) {
-        /** @var UserException $ex */
-        $ex = $e->getPrevious();
-        $logger->error($ex->getMessage(), (array)$ex->getData());
-        exit(1);
+    moveNewStateFile($logger);
+
+    // trigger other exceptions based on the type
+    switch ($e) {
+        case $e instanceof UserException:
+            $logger->error($e->getMessage(), (array)$e->getData());
+            exit(1);
+            break;
+
+
+        case $e instanceof \Keboola\Juicer\Exception\UserException:
+            $logger->error($e->getMessage(), (array)$e->getData());
+            exit(1);
+            break;
+
+
+        case $e instanceof UserException:
+            $logger->error($e->getMessage(), (array)$e->getData());
+            exit($e->getCode() > 1 ? $e->getCode() : 2);
+            break;
+
+        default:
+            if ($e instanceof \GuzzleHttp\Exception\RequestException
+                && $e->getPrevious() instanceof UserException) {
+                /** @var UserException $ex */
+                $ex = $e->getPrevious();
+                $logger->error($ex->getMessage(), (array)$ex->getData());
+                exit(1);
+            }
+            $logger->error(
+                $e->getMessage(),
+                [
+                    'errFile' => $e->getFile(),
+                    'errLine' => $e->getLine(),
+                    'trace' => $e->getTrace(),
+                    'exception' => get_class($e),
+                ]
+            );
+            exit(2);
     }
-    $logger->error($e->getMessage(), [
-        'errFile' => $e->getFile(),
-        'errLine' => $e->getLine(),
-        'trace' => $e->getTrace(),
-        'exception' => get_class($e)
-    ]);
-    exit(2);
 }
 
 $logger->info("Extractor finished successfully.");
